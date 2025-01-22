a66b76c3b614c227229c96050fa9f22abf3287b9
Adam <ahri177511@gmail.com>
2025-01-22 11:29:47 -0700

    Cleaned up Knight movement code

--- shared/src/main/java/chess/king/KingMoves.java
+++ shared/src/main/java/chess/king/KingMoves.java
@@ -3,6 +3,8 @@ import chess.ChessBoard;
 import chess.ChessMove;
 import chess.ChessPiece;
 import chess.ChessPosition;
+import chess.knights.KnightMoves;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.*;
@@ -19,124 +21,52 @@ public class KingMoves {
         // Forward
         if(row < 9){
             ChessPosition new_position = new ChessPosition(row,current_column);
-            ChessPiece another_piece = Board.getPiece(new_position);
-
-            if(another_piece != null){
-                if (selected_piece.getTeamColor() != another_piece.getTeamColor()){
-                    valid_moves.add(new ChessMove(piece_position, new_position, null));
-                }
-            }
-            else{
-                valid_moves.add(new ChessMove(piece_position, new_position, null));
-            }
+            KnightMoves.horse_move(Board, piece_position, selected_piece, valid_moves, new_position);
         }
 
         // Right side
         if(column < 9){
             ChessPosition new_position = new ChessPosition(current_row, column);
-            ChessPiece another_piece = Board.getPiece(new_position);
-
-            if(another_piece != null){
-                if (selected_piece.getTeamColor() != another_piece.getTeamColor()){
-                    valid_moves.add(new ChessMove(piece_position, new_position, null));
-                }
-            }
-            else{
-                valid_moves.add(new ChessMove(piece_position, new_position, null));
-            }
+            KnightMoves.horse_move(Board, piece_position, selected_piece, valid_moves, new_position);
         }
 
         // right top corner
         if(row < 9 && column < 9){
             ChessPosition new_position = new ChessPosition(row,column);
-            ChessPiece another_piece = Board.getPiece(new_position);
-
-            if(another_piece != null){
-                if (selected_piece.getTeamColor() != another_piece.getTeamColor()){
-                    valid_moves.add(new ChessMove(piece_position, new_position, null));
-                }
-            }
-            else{
-                valid_moves.add(new ChessMove(piece_position, new_position, null));
-            }
+            KnightMoves.horse_move(Board, piece_position, selected_piece, valid_moves, new_position);
         }
 
         row = current_row - 1;
         // bottom right corner
         if(row > 0 && column < 9){
             ChessPosition new_position = new ChessPosition(row,column);
-            ChessPiece another_piece = Board.getPiece(new_position);
-
-            if(another_piece != null){
-                if (selected_piece.getTeamColor() != another_piece.getTeamColor()){
-                    valid_moves.add(new ChessMove(piece_position, new_position, null));
-                }
-            }
-            else{
-                valid_moves.add(new ChessMove(piece_position, new_position, null));
-            }
+            KnightMoves.horse_move(Board, piece_position, selected_piece, valid_moves, new_position);
         }
 
         //down movement
         if(row > 0){
             ChessPosition new_position = new ChessPosition(row,current_column);
-            ChessPiece another_piece = Board.getPiece(new_position);
-
-            if(another_piece != null){
-                if (selected_piece.getTeamColor() != another_piece.getTeamColor()){
-                    valid_moves.add(new ChessMove(piece_position, new_position, null));
-                }
-            }
-            else{
-                valid_moves.add(new ChessMove(piece_position, new_position, null));
-            }
+            KnightMoves.horse_move(Board, piece_position, selected_piece, valid_moves, new_position);
         }
 
         column = current_column - 1;
         // Bottom Left Corner
         if(row > 0 && column > 0){
             ChessPosition new_position = new ChessPosition(row,column);
-            ChessPiece another_piece = Board.getPiece(new_position);
-
-            if(another_piece != null){
-                if (selected_piece.getTeamColor() != another_piece.getTeamColor()){
-                    valid_moves.add(new ChessMove(piece_position, new_position, null));
-                }
-            }
-            else{
-                valid_moves.add(new ChessMove(piece_position, new_position, null));
-            }
+            KnightMoves.horse_move(Board, piece_position, selected_piece, valid_moves, new_position);
         }
 
         // left move
         if(column > 0){
             ChessPosition new_position = new ChessPosition(current_row,column);
-            ChessPiece another_piece = Board.getPiece(new_position);
-
-            if(another_piece != null){
-                if (selected_piece.getTeamColor() != another_piece.getTeamColor()){
-                    valid_moves.add(new ChessMove(piece_position, new_position, null));
-                }
-            }
-            else{
-                valid_moves.add(new ChessMove(piece_position, new_position, null));
-            }
+            KnightMoves.horse_move(Board, piece_position, selected_piece, valid_moves, new_position);
         }
 
         row = current_row + 1;
         // top left move
         if(row < 9 && column > 0){
             ChessPosition new_position = new ChessPosition(row,column);
-            ChessPiece another_piece = Board.getPiece(new_position);
-
-            if(another_piece != null){
-                if (selected_piece.getTeamColor() != another_piece.getTeamColor()){
-                    valid_moves.add(new ChessMove(piece_position, new_position, null));
-                }
-            }
-            else{
-                valid_moves.add(new ChessMove(piece_position, new_position, null));
-            }
+            KnightMoves.horse_move(Board, piece_position, selected_piece, valid_moves, new_position);
         }
 
 
--- shared/src/main/java/chess/knights/KnightMoves.java
+++ shared/src/main/java/chess/knights/KnightMoves.java
@@ -12,23 +12,13 @@ public class KnightMoves {
         List<ChessMove> valid_moves = new ArrayList<>();
         int current_row = piece_position.getRow();
         int current_column = piece_position.getColumn();
-        int row = current_row;
-        int column = current_column;
 
         // reverse 7 move
-        row = current_row + 2;
-        column = current_column + 1;
+        int row = current_row + 2;
+        int column = current_column + 1;
         ChessPosition new_position_1 = new ChessPosition(row, column);
         if(row <= 8 && column < 8){
-            ChessPiece another_piece = Board.getPiece(new_position_1);
-            if(another_piece != null){
-                if(selected_piece.getTeamColor() != another_piece.getTeamColor()){
-                    valid_moves.add(new ChessMove(piece_position, new_position_1, null));
-                }
-            }
-            else{
-                valid_moves.add(new ChessMove(piece_position, new_position_1, null));
-            }
+            horse_move(Board, piece_position, selected_piece, valid_moves, new_position_1);
         }
 
 
@@ -37,15 +27,7 @@ public class KnightMoves {
         column = current_column - 1;
         ChessPosition new_position_2 = new ChessPosition(row, column);
         if(row <=8  && column >= 1){
-            ChessPiece another_piece_2 = Board.getPiece(new_position_2);
-            if(another_piece_2 != null){
-                if(selected_piece.getTeamColor() != another_piece_2.getTeamColor()){
-                    valid_moves.add(new ChessMove(piece_position, new_position_2, null));
-                }
-            }
-            else{
-                valid_moves.add(new ChessMove(piece_position, new_position_2, null));
-            }
+            horse_move(Board, piece_position, selected_piece, valid_moves, new_position_2);
         }
 
 
@@ -54,15 +36,7 @@ public class KnightMoves {
         column = current_column + 2;
         ChessPosition new_position_3 = new ChessPosition(row, column);
         if(row <= 8 && column <=8){
-            ChessPiece another_piece_3 = Board.getPiece(new_position_3);
-            if(another_piece_3 != null){
-                if(selected_piece.getTeamColor() != another_piece_3.getTeamColor()){
-                    valid_moves.add(new ChessMove(piece_position, new_position_3, null));
-                }
-            }
-            else{
-                valid_moves.add(new ChessMove(piece_position, new_position_3, null));
-            }
+            horse_move(Board, piece_position, selected_piece, valid_moves, new_position_3);
         }
 
 
@@ -71,15 +45,7 @@ public class KnightMoves {
         column = current_column - 2;
         ChessPosition new_position_4 = new ChessPosition(row, column);
         if(row <= 8 && column >=1){
-            ChessPiece another_piece_4 = Board.getPiece(new_position_4);
-            if(another_piece_4 != null){
-                if(selected_piece.getTeamColor() != another_piece_4.getTeamColor()){
-                    valid_moves.add(new ChessMove(piece_position, new_position_4, null));
-                }
-            }
-            else{
-                valid_moves.add(new ChessMove(piece_position, new_position_4, null));
-            }
+            horse_move(Board, piece_position, selected_piece, valid_moves, new_position_4);
         }
 
 
@@ -88,15 +54,7 @@ public class KnightMoves {
         column = current_column + 1;
         ChessPosition new_position_5 = new ChessPosition(row, column);
         if(row >= 1 && column <=8){
-            ChessPiece another_piece_5 = Board.getPiece(new_position_5);
-            if(another_piece_5 != null){
-                if(selected_piece.getTeamColor() != another_piece_5.getTeamColor()){
-                    valid_moves.add(new ChessMove(piece_position, new_position_5, null));
-                }
-            }
-            else{
-                valid_moves.add(new ChessMove(piece_position, new_position_5, null));
-            }
+            horse_move(Board, piece_position, selected_piece, valid_moves, new_position_5);
         }
 
         // reverse L move
@@ -104,15 +62,7 @@ public class KnightMoves {
         column = current_column - 1;
         ChessPosition new_position_6 = new ChessPosition(row, column);
         if(row >= 1 && column >=1){
-            ChessPiece another_piece_6 = Board.getPiece(new_position_6);
-            if(another_piece_6 != null){
-                if(selected_piece.getTeamColor() != another_piece_6.getTeamColor()){
-                    valid_moves.add(new ChessMove(piece_position, new_position_6, null));
-                }
-            }
-            else{
-                valid_moves.add(new ChessMove(piece_position, new_position_6, null));
-            }
+            horse_move(Board, piece_position, selected_piece, valid_moves, new_position_6);
         }
 
 
@@ -121,15 +71,7 @@ public class KnightMoves {
         column = current_column + 2;
         ChessPosition new_position_7 = new ChessPosition(row, column);
         if(row >= 1 && column <=8){
-            ChessPiece another_piece_7 = Board.getPiece(new_position_7);
-            if(another_piece_7 != null){
-                if(selected_piece.getTeamColor() != another_piece_7.getTeamColor()){
-                    valid_moves.add(new ChessMove(piece_position, new_position_7, null));
-                }
-            }
-            else{
-                valid_moves.add(new ChessMove(piece_position, new_position_7, null));
-            }
+            horse_move(Board, piece_position, selected_piece, valid_moves, new_position_7);
         }
 
 
@@ -138,19 +80,23 @@ public class KnightMoves {
         column = current_column - 2;
         ChessPosition new_position_8 = new ChessPosition(row, column);
         if(row >= 1 && column >= 1){
-            ChessPiece another_piece_8 = Board.getPiece(new_position_8);
-            if(another_piece_8 != null){
-                if(selected_piece.getTeamColor() != another_piece_8.getTeamColor()){
-                    valid_moves.add(new ChessMove(piece_position, new_position_8, null));
-                }
-            }
-            else{
-                valid_moves.add(new ChessMove(piece_position, new_position_8, null));
-            }
+            horse_move(Board, piece_position, selected_piece, valid_moves, new_position_8);
         }
 
 
 
         return valid_moves;
+    }
+
+    public static void horse_move(ChessBoard Board, ChessPosition piece_position, ChessPiece selected_piece, List<ChessMove> valid_moves, ChessPosition new_position_1) {
+        ChessPiece another_piece = Board.getPiece(new_position_1);
+        if(another_piece != null){
+            if(selected_piece.getTeamColor() != another_piece.getTeamColor()){
+                valid_moves.add(new ChessMove(piece_position, new_position_1, null));
+            }
+        }
+        else{
+            valid_moves.add(new ChessMove(piece_position, new_position_1, null));
+        }
     }
 }
