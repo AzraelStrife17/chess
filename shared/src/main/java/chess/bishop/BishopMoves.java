a3b62aaf4d01eee0e8ea55384e6d4ce0f0004184
Adam <ahri177511@gmail.com>
2025-02-24 10:24:16 -0700

    simplified bishop code

--- shared/src/main/java/chess/bishop/BishopMoves.java
+++ shared/src/main/java/chess/bishop/BishopMoves.java
@@ -15,54 +15,39 @@ public class BishopMoves {
         int row = currentRow;
 
         for (int column = currentColumn + 1; column < 9; column++) {
-            row ++;
+            row++;
             if(row < 9) {
-                ChessPosition newPosition = new ChessPosition(row, column);
-                ChessPiece anotherPiece = board.getPiece(newPosition);
-                checkForOtherPieces(piecePosition, selectedPiece, validMoves, newPosition, anotherPiece);
-                if (anotherPiece != null) {
+                if (bishopMoves(board, piecePosition, selectedPiece, validMoves, column, row)) {
                     break;
                 }
             }
         }
-
         row = currentRow;
 
         for (int column = currentColumn - 1; column > 0; column--) {
-            row ++;
+            row++;
             if(row < 9) {
-                ChessPosition newPosition = new ChessPosition(row, column);
-                ChessPiece anotherPiece = board.getPiece(newPosition);
-                checkForOtherPieces(piecePosition, selectedPiece, validMoves, newPosition, anotherPiece);
-                if (anotherPiece != null) {
+                if (bishopMoves(board, piecePosition, selectedPiece, validMoves, column, row)) {
                     break;
                 }
             }
         }
-
         row = currentRow;
 
         for (int column = currentColumn + 1; column < 9; column++) {
             row --;
             if (row > 0) {
-                ChessPosition newPosition = new ChessPosition(row, column);
-                ChessPiece anotherPiece = board.getPiece(newPosition);
-                checkForOtherPieces(piecePosition, selectedPiece, validMoves, newPosition, anotherPiece);
-                if (anotherPiece != null) {
+                if (bishopMoves(board, piecePosition, selectedPiece, validMoves, column, row)) {
                     break;
                 }
             }
         }
-
         row = currentRow;
 
         for (int column = currentColumn - 1; column > 0; column--) {
             row --;
             if (row > 0) {
-                ChessPosition newPosition = new ChessPosition(row, column);
-                ChessPiece anotherPiece = board.getPiece(newPosition);
-                checkForOtherPieces(piecePosition, selectedPiece, validMoves, newPosition, anotherPiece);
-                if (anotherPiece != null) {
+                if (bishopMoves(board, piecePosition, selectedPiece, validMoves, column, row)) {
                     break;
                 }
             }
@@ -70,15 +55,20 @@ public class BishopMoves {
         return validMoves;
     }
 
-    private static void checkForOtherPieces(ChessPosition piecePosition, ChessPiece selectedPiece, List<ChessMove> validMoves, ChessPosition newPosition, ChessPiece anotherPiece) {
-        if(anotherPiece != null) {
-            if(selectedPiece.getTeamColor() != anotherPiece.getTeamColor()) {
+
+    private static boolean bishopMoves(ChessBoard Board, ChessPosition piecePosition, ChessPiece selectedPiece, List<ChessMove> validMoves, int column, int row) {
+            ChessPosition newPosition = new ChessPosition(row, column);
+            ChessPiece anotherPiece = Board.getPiece(newPosition);
+            if (anotherPiece != null) {
+                if (anotherPiece.getTeamColor() != selectedPiece.getTeamColor()) {
+                    validMoves.add(new ChessMove(piecePosition, newPosition, null));
+                }
+                return true;
+            } else {
                 validMoves.add(new ChessMove(piecePosition, newPosition, null));
             }
-        }
-        else {
-            validMoves.add(new ChessMove(piecePosition, newPosition, null));
-        }
-    }}
+            return false;
+    }
+}
 
 
--- shared/src/main/java/chess/rook/RookMoves.java
+++ shared/src/main/java/chess/rook/RookMoves.java
@@ -8,42 +8,42 @@ import java.util.List;
 import java.util.*;
 
 public class RookMoves {
-    public static Collection<ChessMove> rookMovesCalculator(ChessBoard Board, ChessPosition piecePosition, ChessPiece selectedPiece){
+    public static Collection<ChessMove> rookMovesCalculator(ChessBoard board, ChessPosition piecePosition, ChessPiece selectedPiece){
         List<ChessMove> validMoves = new ArrayList<>();
         int currentRow = piecePosition.getRow();
         int currentColumn = piecePosition.getColumn();
 
         // Horizontal Movement
         for (int column = currentColumn + 1; column < 9; column++) {
-            if (rookMoves(Board, piecePosition, selectedPiece, validMoves, column, currentRow)) {
+            if (rookMoves(board, piecePosition, selectedPiece, validMoves, column, currentRow)) {
                 break;
             }
         }
 
         for (int column = currentColumn - 1; column > 0; column--) {
-            if (rookMoves(Board, piecePosition, selectedPiece, validMoves, column, currentRow)) {
+            if (rookMoves(board, piecePosition, selectedPiece, validMoves, column, currentRow)) {
                 break;
             }
         }
 
         // Vertical movement
         for (int row = currentRow + 1; row < 9; row++) {
-            if (rookMoves(Board, piecePosition, selectedPiece, validMoves, currentColumn, row)) {
+            if (rookMoves(board, piecePosition, selectedPiece, validMoves, currentColumn, row)) {
                 break;
             }
         }
 
         for (int row = currentRow - 1; row > 0; row--) {
-            if (rookMoves(Board, piecePosition, selectedPiece, validMoves, currentColumn, row)) {
+            if (rookMoves(board, piecePosition, selectedPiece, validMoves, currentColumn, row)) {
                 break;
             }
         }
         return validMoves;
     }
 
-    private static boolean rookMoves(ChessBoard Board, ChessPosition piecePosition, ChessPiece selectedPiece, List<ChessMove> validMoves, int column, int row) {
+    private static boolean rookMoves(ChessBoard board, ChessPosition piecePosition, ChessPiece selectedPiece, List<ChessMove> validMoves, int column, int row) {
         ChessPosition newPosition = new ChessPosition(row, column);
-        ChessPiece anotherPiece = Board.getPiece(newPosition);
+        ChessPiece anotherPiece = board.getPiece(newPosition);
         if (anotherPiece != null) {
             if (anotherPiece.getTeamColor() != selectedPiece.getTeamColor()) {
                 validMoves.add(new ChessMove(piecePosition, newPosition, null));
